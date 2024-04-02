868663a990da6ec075670ad0c6dc4b3d852fcc35
Hassan Gaber <hassansameh90@gmail.com>
2024-04-01 20:26:24 -0400

    Greedy algorithm works but non-optimal

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ### Description
 
+The main objective of this assignment is to compare a brute-force and guided search approach to solving a network optimization problem. The problem's constraints include a maximum cost constraint, a node constraint, and the objective of maximizing network overall reliability. The two approaches should yield the same solutions with the guided search arriving at the optimal graph faster than the brute-force method.
+
+The brute-force algorithm generates the optimal graph using the following approach: 1. Generate all the possible graphs given the number of nodes and full connectivity of the graphs 2. Filter the graphs based on the edge cost and maximum cost constraint 3. Compute all graph reliabilities and choose the highest reliability graph.
+
+The guided search algorithm finds the optimal graph using the following: 
+
 ### Code Structure
 
 ### How to Run
--- src/main.py
+++ src/main.py
@@ -1,24 +1,27 @@
-import numpy as np
+import time
 from design import NetworkDesigner
 from read_input import read_network_file
+
 
 FILES=['data/4_city.txt','data/4_city.txt','data/5_city.txt', 'data/5_city.txt', 'data/6_city.txt','data/6_city.txt']
 MAX_COSTS=[50,60,60,70,65,85] 
 
 def main():
     
-    for file, max_cost in zip(FILES, MAX_COSTS):
-        num_cities, reliability_matrix, cost_matrix = read_network_file(file)
+    # for file, max_cost in zip(FILES, MAX_COSTS):
+    #     num_cities, reliability_matrix, cost_matrix = read_network_file(file)
+    #     designer = NetworkDesigner(num_cities-1, cost_matrix, reliability_matrix)
+    #     start = time.monotonic()
+    #     designer.fit_transform_part_1(max_cost=max_cost, reliability_simulations=10000)
+    #     end = time.monotonic()
+    #     print(f'Simple solution runtime for file={file} max_cost={max_cost} : {end - start: .2f} s')
 
-        designer = NetworkDesigner(num_cities-1, cost_matrix, reliability_matrix)
+    file=FILES[4]
+    num_cities, reliability_matrix, cost_matrix = read_network_file(file)
 
-        designer.fit_transform_part_1(max_cost=max_cost)
-    # file=FILES[0]
-    # num_cities, reliability_matrix, cost_matrix = read_network_file(file)
+    designer = NetworkDesigner(num_cities, cost_matrix, reliability_matrix)
 
-    # designer = NetworkDesigner(num_cities-1, cost_matrix, reliability_matrix)
-
-    # designer.fit_transform_part_2(max_cost=120)
+    designer.fit_transform(max_cost=60)
 
 if __name__ == "__main__":
     main()
